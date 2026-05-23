8b3cc7e56063278edc314a44a70ccbc06cddf481
lhnhidev <lhnhi420@gmail.com>
2026-05-23 18:07:37 +0700

    refactor: rename User.CreatedAt column to created_at (snake_case)
    Added migration to rename "CreatedAt" to "created_at" in "users" table for naming consistency. Updated Designer and ModelSnapshot files to reflect this change for Entity Framework. (#9)

---
+++ apps/backend/src/GreenEcoCommerce.Infrastructure/Migrations/20260523105935_UpdateUser-AdditionalCreatedAtField.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GreenEcoCommerce.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserAdditionalCreatedAtField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                schema: "public",
+                table: "users",
+                newName: "created_at");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "created_at",
+                schema: "public",
+                table: "users",
+                newName: "CreatedAt");
+        }
+    }
+}
