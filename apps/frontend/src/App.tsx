c6c130a48d5a9fb941d251cba835fce25fb678fe
nthung-2k5 <hungthitkhia@gmail.com>
2026-05-24 16:16:01 +0700

    feat: implement the homepage file

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,5 +1,16 @@
-const App = () => {
-  return <div className="text-3xl underline">Hello</div>
-}
+import { Route, Switch } from 'wouter'
+import { HomePage } from './components/HomePage'
+import { Navigation } from './components/Navigation'
 
-export default App
+export default function App() {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navigation />
+      <main>
+        <Switch>
+          <Route path="/" component={HomePage} />
+        </Switch>
+      </main>
+    </div>
+  )
+}
