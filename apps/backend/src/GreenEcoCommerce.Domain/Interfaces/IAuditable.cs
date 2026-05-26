fd296a6056a04ffbc70a6c8086a6ad9a657b42cf
nthung-2k5 <hungthitkhia@gmail.com>
2026-05-26 15:43:22 +0700

    feat: add auditable interfaces (#9)

---
+++ apps/backend/src/GreenEcoCommerce.Domain/Interfaces/IAuditable.cs
@@ -0,0 +1,11 @@
+﻿namespace GreenEcoCommerce.Domain.Interfaces;
+
+public interface IHasCreatedAt
+{
+    DateTimeOffset CreatedAt { get; set; }
+}
+
+public interface IHasUpdatedAt
+{
+    DateTimeOffset? UpdatedAt { get; set; }
+}
