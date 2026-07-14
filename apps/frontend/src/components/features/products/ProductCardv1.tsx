c3511dc189c9c1d184c15190b20ab9c2528b9052
nthung-2k5 <hungthitkhia@gmail.com>
2026-07-14 15:56:13 +0700

    hotfix: fix incorrect import path

--- apps/frontend/src/components/features/products/ProductCardv1.tsx
+++ apps/frontend/src/components/features/products/ProductCardv1.tsx
@@ -1,7 +1,7 @@
 import type { ProductDto } from '@api/schemas'
+import { ImageWithFallback } from '@components/ui/status/ImageWithFallback'
 import { Badge, Button, Card } from '@mantine/core'
 import { StarIcon } from '@phosphor-icons/react'
-import { ImageWithFallback } from '@ui/status/ImageWithFallback'
 
 const ProductCardv1 = ({ product }: { product: ProductDto }) => {
   return (
