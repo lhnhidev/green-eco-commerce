2a0ac729eb64998e02b3c3961f85d66ba47cb020
nthung-2k5 <hungthitkhia@gmail.com>
2026-07-19 17:09:25 +0700

    refine: Profile feature

---
+++ apps/backend/src/GreenEcoCommerce.Application/Features/Profile/UserProfileDto.cs
@@ -0,0 +1,23 @@
+﻿using GreenEcoCommerce.Domain.Entities;
+using GreenEcoCommerce.Domain.Enums;
+using Riok.Mapperly.Abstractions;
+
+namespace GreenEcoCommerce.Application.Features.Profile;
+
+public record UserProfileDto(
+    string Avatar,
+    string FirstName,
+    string LastName,
+    string Email,
+    string Phone,
+    string Address,
+    RoleEnum Role,
+    DateTimeOffset CreatedAt
+);
+
+[Mapper(RequiredMappingStrategy = RequiredMappingStrategy.Target)]
+public static partial class ProfileDtoMapper
+{
+    public static partial UserProfileDto ToProfileDto(this User profile);
+    public static partial IQueryable<UserProfileDto> ProjectToProfileDto(this IQueryable<User> q);
+}
