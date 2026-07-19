6ed5758a30909fce28a18c752b4213e0f22ec5f4
nthung-2k5 <hungthitkhia@gmail.com>
2026-07-19 16:18:06 +0700

    migration: migrate to .NET Aspire and local DB instead of Supabase

---
+++ apps/backend/src/GreenEcoCommerce.AppHost/AppHost.cs
@@ -0,0 +1,14 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var postgres = builder.AddPostgres("postgres").WithDataVolume(isReadOnly: false);
+var db = postgres.AddDatabase("GreenEcoCommerce-DB", "green_eco_commerce");
+
+var cache = builder.AddRedis("cache")
+        .WithRedisInsight();
+
+builder.AddProject<Projects.GreenEcoCommerce_WebAPI>("greenecocommerce-webapi")
+        .WithReference(db)
+        .WithReference(cache)
+        .WaitFor(db);
+
+builder.Build().Run();
