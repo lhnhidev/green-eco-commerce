14f022c8c6a89ac3b8772afdd5bbd761ba42dda2
nthung-2k5 <hungthitkhia@gmail.com>
2026-07-26 00:18:19 +0700

    fix: fix error where cannot retrieve green wallet

--- apps/backend/src/GreenEcoCommerce.WebAPI/Endpoints/GreenWalletEndpoints.cs
+++ apps/backend/src/GreenEcoCommerce.WebAPI/Endpoints/GreenWalletEndpoints.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using GreenEcoCommerce.Application.Features.GreenWallets;
 using GreenEcoCommerce.Application.Features.GreenWallets.Queries;
 using MediatR;
@@ -9,14 +10,15 @@ public static class GreenWalletEndpoints
 {
     public static void MapGreenWalletEndpoints(this WebApplication app)
     {
-        var group = app.MapGroup("/api/green-wallets/{userId:guid}").WithTags("GreenWallets")
+        var group = app.MapGroup("/api/green-wallets").WithTags("GreenWallets")
             .ProducesProblem(StatusCodes.Status500InternalServerError);
 
         group.MapGet("/", GetGreenWallet);
     }
 
-    private static async Task<Ok<GreenWalletDto>> GetGreenWallet(Guid userId, ISender sender)
+    private static async Task<Ok<GreenWalletDto>> GetGreenWallet(ClaimsPrincipal user, ISender sender)
     {
+        var userId = Guid.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? Guid.Empty.ToString());
         var greenWallet = await sender.Send(new GetUserGreenWalletQuery(userId));
         return TypedResults.Ok(greenWallet);
     }
